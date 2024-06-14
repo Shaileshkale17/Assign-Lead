757434ee039540dd93e1e94c5703b9e62b071873
Shailesh kale <shaileshkale87730@gmail.com>
2024-06-14 12:03:19 +0530

    first commit

---
+++ src/index.js
@@ -0,0 +1,23 @@
+import dotenv from "dotenv";
+import { ConnectDB } from "./db/index.js";
+import { app } from "./app.js";
+
+dotenv.config({
+  path: "./env",
+});
+
+const port = process.env.PORT || 3001;
+
+ConnectDB()
+  .then(() => {
+    app.listen(port, () => {
+      console.log(`Connecting to ${port}`);
+    });
+    app.on("error", (error) => {
+      console.log(`Error: ${error}`);
+      throw error;
+    });
+  })
+  .catch((error) => {
+    console.log(`MONGO DB CONNECTION ERROR: ${error}`);
+  });
